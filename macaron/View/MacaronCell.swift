fba2533b26de3c81a1315f0e5bad7f52a4b961a4
Ryosuke Hayashi <hayashi@hayashikun.com>
2017-08-26 18:22:31 +0900

    Add cell classes and protocols

---
+++ macaron/View/MacaronCell.swift
@@ -0,0 +1,11 @@
+//
+//  MacaronCell.swift
+//  macaron
+//
+//  Created by Ryosuke Hayashi on 2017/08/26.
+//  Copyright © 2017年 yagra. All rights reserved.
+//
+
+import UIKit
+
+public class MacaronCell: UITableViewCell {}
