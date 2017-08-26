8b95b2ed9f1e83f6cc6e86f2753d4fe4b8729b53
Ryosuke Hayashi <hayashi@hayashikun.com>
2017-08-26 16:19:28 +0900

    first commit

---
+++ macaron/macaron.h
@@ -0,0 +1,19 @@
+//
+//  macaron.h
+//  macaron
+//
+//  Created by Ryosuke Hayashi on 2017/08/26.
+//  Copyright © 2017年 yagra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for macaron.
+FOUNDATION_EXPORT double macaronVersionNumber;
+
+//! Project version string for macaron.
+FOUNDATION_EXPORT const unsigned char macaronVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <macaron/PublicHeader.h>
+
+
