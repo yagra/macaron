3c55a38916d9921210bc2346e1ec2bca7718401d
Ryosuke Hayashi <hayashi@hayashikun.com>
2017-08-27 17:35:42 +0900

    Management using protocol is difficult. So, change mind VC class manage macaron.

---
+++ macaron/MacaronViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MacaronViewController.swift
+//  macaron
+//
+//  Created by Ryosuke Hayashi on 2017/08/26.
+//  Copyright © 2017年 yagra. All rights reserved.
+//
+
+import UIKit
+
+open class MacaronViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    private var macaronTableView: UITableView!
+    private var macaronCellData: [MacaronCellDataType] = []
+    private var cellHeight: [CGFloat] = []
+
+    public func initialize(tableView: UITableView, cellData: [MacaronCellDataType]) {
+        macaronTableView = tableView
+        macaronTableView.delegate = self
+        macaronTableView.dataSource = self
+        macaronCellData = cellData
+        cellHeight = [CGFloat](repeating: 0.0, count: cellData.count)
+        for cell in Set(cellData.map{$0.CellClassName}) {
+            tableView.register(UINib(nibName: cell, bundle: Config.MacaronBundle), forCellReuseIdentifier: cell)
+        }
+    }
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return macaronCellData.count
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var data = macaronCellData[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: data.CellClassName,
+                                                 for: indexPath) as! MacaronCellType
+        cell.assign(data: &data)
+        cellHeight[indexPath.row] = cell.getRowHeight()
+        return cell as! UITableViewCell
+    }
+
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return cellHeight[indexPath.row]
+    }
+}
