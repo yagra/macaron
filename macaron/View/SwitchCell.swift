87a83297a6db3de99081b47cc2614c101e56f476
Ryosuke Hayashi <hayashi@hayashikun.com>
2017-09-02 17:02:57 +0900

    Add SwitchCell

---
+++ macaron/View/SwitchCell.swift
@@ -0,0 +1,41 @@
+//
+//  SwitchCell.swift
+//  macaron
+//
+//  Created by Ryosuke Hayashi on 2017/09/02.
+//  Copyright © 2017年 yagra. All rights reserved.
+//
+
+import Foundation
+
+public class SwitchCell: MacaronCell, MacaronInputCellType {
+    private var data: SwitchCellData!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var toggle: UISwitch!
+
+    public func getRowHeight() -> CGFloat {
+        return 45
+    }
+
+    public func assign(data: inout MacaronCellDataType) {
+        if let data = data as? SwitchCellData {
+            self.data = data
+            label.text = data.labelText
+            label.textAlignment = data.labelTextAlignment
+            toggle.isOn = data.value.value
+        }
+    }
+
+    @IBAction func switchValueChanged(_ sender: Any) {
+        self.data.value.value = toggle.isOn
+    }
+}
+
+public class SwitchCellData: MacaronInputCellData<SwitchCell, BoolValue> {
+    public var labelTextAlignment: NSTextAlignment
+
+    public init(labelText: String, value: InputType, labelTextAlignment: NSTextAlignment=NSTextAlignment.center) {
+        self.labelTextAlignment = labelTextAlignment
+        super.init(labelText: labelText, value: value)
+    }
+}
