7d98c81405cc93e9665ca1a7250216047241b172
Ryosuke Hayashi <hayashi@hayashikun.com>
2017-10-09 13:14:31 +0900

    Enable to set horizontal space

--- macaron/View/LineCell.swift
+++ macaron/View/LineCell.swift
@@ -10,6 +10,10 @@ import Foundation
 
 public class LineCell: MacaronCell, MacaronCellType {
     var lineHeight: CGFloat = 1.0
+    @IBOutlet weak var leftSpaceConstraint: NSLayoutConstraint!
+    @IBOutlet weak var rightSpaceConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bodyView: UIView!
+    
     public func getRowHeight() -> CGFloat {
         return lineHeight
     }
@@ -17,7 +21,9 @@ public class LineCell: MacaronCell, MacaronCellType {
     public func assign(data: inout MacaronCellDataType) {
         if let data = data as? LineCellData {
             lineHeight = data.height
-            contentView.backgroundColor = data.color
+            leftSpaceConstraint.constant = data.leftSpace
+            rightSpaceConstraint.constant = data.rightSpace
+            bodyView.backgroundColor = data.color
         }
     }
 }
@@ -25,6 +31,8 @@ public class LineCell: MacaronCell, MacaronCellType {
 public class LineCellData: MacaronCellData<LineCell> {
     public var height: CGFloat
     public var color: UIColor
+    public var leftSpace: CGFloat = 0.0
+    public var rightSpace: CGFloat = 0.0
 
     public init(height: CGFloat, color: UIColor) {
         self.height = height
